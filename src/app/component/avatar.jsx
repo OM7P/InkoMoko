e70d6b9ef3023e68e795d851e6d3f6ca22e0aebf
omprakash-boppo <omprakash.prajapati@boppotechnologies.com>
2024-10-03 17:17:00 +0530

    Create inkokomo web site

---
+++ src/app/component/avatar.jsx
@@ -0,0 +1,17 @@
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+  } from "../../components/ui/avatar"
+  
+  export function AvatarDemo() {
+    return (
+      <Avatar>
+        <AvatarImage src={"/images/avatar.jpg"} alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+
+      
+    )
+  }
+  
---
+++ src/app/component/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Footer() {
+  return (
+    <div className="absolute top-[880px] px-1 w-full flex  justify-center items-center">
+      <div className="text-gray-900">
+
+      Copyright © 2014–2024 InkoMoko Ltd. All rights reserved.
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
